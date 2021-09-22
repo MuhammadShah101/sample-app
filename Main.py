73a4e04b6547fe54b8f93c47a55b6a9adbafb3e8
Daniel <drueda2000@gmail.com>
2021-09-22 09:03:31 -0700

    Main python change-daniel

--- Main.py
+++ Main.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import (
+    Flask, 
+    render_template
+)
 
 app = Flask(__name__)
 
-@app.route('/') #decorator
-def hello():
-    return render_template("hello.html")
-
-app.run(use_reloader=True)
+@app.route('/')
+@app.route('/index') #decorator
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Texas!'
+        },
+        {
+            'author': {'username': 'James'},
+            'body': 'Coding, Coding, CODING, COdInG!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
