5b65a43852c965ab582e4af2f8c7bebd8323ac6b
Daniel <drueda2000@gmail.com>
2021-09-22 11:50:34 -0700

    chapter 3 + a little of chapter 4

--- Main.py
+++ Main.py
@@ -1,20 +1,30 @@
 from forms import LoginForm
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask import (
-    Flask, 
+    Flask,
+    flash,
     render_template,
-    redirect
+    redirect,
+    url_for
 )
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db =SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/index') #decorator
